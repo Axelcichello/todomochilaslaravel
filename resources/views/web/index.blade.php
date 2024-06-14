29df42daad7470920fad6c59fcb31351d1144b12
Axel Nahuel Cichello <csaxel49@gmail.com>
2024-06-13 22:13:50 -0300

    se crea la ruta index, el controlador BackpackController y se crean las carpetas para guardar las vistas

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>TodoMochilas</title>
+</head>
+
+<header class="py-5 bg-black">
+
+    <div class="container mx-auto flex justify-between items-center">
+        <p class=" text-white text-3xl">Todo<span class=" text-orange-600 font-bold">Mochilas</span></p>
+
+        <nav>
+            <a href="#" class="text-white px-3">Categorias</a>
+            <a href="#" class="text-white px-3">Empresa</a>
+            <a href="#" class="text-white px-3">Galeria</a>
+            <a href="#" class="text-white px-3">Nosotros</a>
+        </nav>
+    </div>
+
+</header>
+
+<body>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackpackController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BackpackController::class, 'index']);
